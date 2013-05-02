60e8426b11b264a210114690b30393ed042d8f54
631220026 <631220026@Sala603-14.fspoaeduc.com.br>
2013-05-02 19:43:21 -0300

    trabalho9

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorI.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorI.java
@@ -5,9 +5,18 @@ import java.util.Comparator;
 public class MeuComparatorI implements Comparator<Integer> {
 
 	@Override
-	public int compare(Integer arg0, Integer arg1) {
+	public int compare(Integer o1, o2) {
 		
-		
+		if  (par(o1) == par(o2)){
+			int resultado = 0;
+			
+			else {
+				
+				resultado = compareParidadeDistinta (o1,o2);
+				
+			}
+			return resultado;
+		}
 		
 		return 0;
 	}
--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorS.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorS.java
@@ -5,11 +5,15 @@ import java.util.Comparator;
 public class MeuComparatorS implements Comparator<String>{
 
 	@Override
-	public int compare(String arg0, String arg1) {
+	public int compare(String o1, String o2) {
 		
-			return 0;
+		int resultado = 0;
+		
+		resultado = 0 - o2.compareTo(o2);
+		
+		
+			return  resultado;
 	}
 
-		
 
 }
